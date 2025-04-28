9249e74b89897cccae1c37321ef755d9001a0c92
aravindav <aravindwaitz@gmail.com>
2025-04-28 03:17:47 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,11 +21,11 @@ app.post('/api/todos', (req, res) => {
 });
 
 // Serve static files from React app
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, 'client-build')));
 
 // Catch-all to serve React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client-build', 'index.html'));
 });
 
 app.listen(PORT, () => {
